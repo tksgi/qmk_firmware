0175c537e0393f2776e2d8e0c94a11df95365940
swanmatch <swan_match@yahoo.co.jp>
2020-03-19 18:52:39 +0900

    tmp

--- keyboards/yosino58/keymaps/swan/keymap.c
+++ keyboards/yosino58/keymaps/swan/keymap.c
@@ -105,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   RESET,   KC_F1,   KC_F2,      KC_F3,   KC_F4,      KC_F5,                                        KC_F6,   KC_F7,        KC_F8,   KC_F9,        KC_F10,  RESET,   \
   RGBRST,  KC_F1,   KC_F2,      KC_PGUP, KC_F4,      KC_F5,                                        KC_F6,   KC_F7,        KC_UP,   KC_F9,        KC_F10,  RGBRST,  \
   _______, KC_TILD, KC_HOME,    KC_PGDN, KC_END,     KC_LPRN,                                      KC_RPRN, KC_LEFT,      KC_DOWN, KC_RGHT,      KC_PIPE, KC_F11,  \
-  _______, KC_GRV,  C(KC_LEFT), KC_F3,   C(KC_RGHT), KC_LPRN, KC_LPRN,   ADJUST, _______, KC_RPRN, KC_RPRN, LCA(KC_LEFT), KC_F8,   LCA(KC_RGHT), KC_BSLS, _______, \
+  _______, KC_GRV,  C(KC_LEFT), KC_F3,   C(KC_RGHT), KC_LPRN, KC_LPRN,   ADJUST, _______, KC_RPRN, KC_RPRN, LCA(KC_LEFT), KC_F8,   LCA(KC_RGHT), KC_BSLS, KC_PSCR, \
                                          _______,    _______, C(KC_SPC),                  _______, _______, _______ \
   ),
 
