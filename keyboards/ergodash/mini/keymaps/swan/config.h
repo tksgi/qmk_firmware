43c69926e77f7a2e8fa91204234cf20584a52fcd
swanmatch <swan_match@yahoo.co.jp>
2020-05-26 09:31:22 +0900

    keymap adjust

--- keyboards/ergodash/mini/keymaps/swan/config.h
+++ keyboards/ergodash/mini/keymaps/swan/config.h
@@ -34,6 +34,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
 #define RGBLED_NUM 20
-#define RGBLIGHT_HUE_STEP 10
-#define RGBLIGHT_SAT_STEP 17
-#define RGBLIGHT_VAL_STEP 17
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 5
+#define RGBLIGHT_VAL_STEP 8
+#define RGBLIGHT_EFFECT_SNAKE_LENGTH RGBLED_NUM
--- keyboards/ergodash/mini/keymaps/swan/keymap.c
+++ keyboards/ergodash/mini/keymaps/swan/keymap.c
@@ -62,10 +62,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT( \
-    KC_ESC,  KC_Q,    KC_W,    KC_E,          KC_R,    KC_T,           KC_LPRN,  KC_RPRN, KC_Y,          KC_U,   KC_I,    KC_O,    KC_P,    KC_BSLS, \
+    KC_ESC,  KC_Q,    KC_W,    KC_E,          KC_R,    KC_T,           KC_LPRN,  KC_RPRN, KC_Y,          KC_U,   KC_I,    KC_O,    KC_P,    KC_EQL,  \
     KC_TAB,  KC_A,    KC_S,    KC_D,          KC_F,    KC_G,           KC_LBRC,  KC_RBRC, KC_H,          KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,          KC_V,    KC_B,           KC_NO ,   KC_NO ,  KC_N,          KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    KC_LCTL, KC_NO,   KC_LGUI, LALT_T(KC_F5), KC_BSPC, LSFT_T(KC_SPC), CALC,     CUSL,    LCTL_T(KC_ENT), KC_DEL, KC_F12,  KC_APP,  KC_QUES, KC_PERC  \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,          KC_V,    KC_B,           KC_NO ,   KC_NO ,  KC_N,          KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_MINS, \
+    KC_LCTL, KC_NO,   KC_LGUI, LALT_T(KC_F5), KC_BSPC, LSFT_T(KC_SPC), CALC,     CUSL,    LCTL_T(KC_ENT), KC_DEL, KC_F12,  KC_APP,  KC_QUES, KC_MINS  \
   ),
 
   /* Lower
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_LOWER] = LAYOUT(
   _______, KC_F1,   KC_F2,      KC_PGUP, KC_F4,      KC_F5,   _______,    _______, KC_F6,   KC_F7,        KC_UP,   KC_F9,        KC_F10,  KC_PSCR, \
-  _______, KC_TILD, KC_HOME,    KC_PGDN, KC_END,     KC_LCBR, _______,    _______, KC_RCBR, KC_LEFT,      KC_DOWN, KC_RGHT,      KC_PIPE, KC_F11,  \
+  _______, KC_TILD, KC_HOME,    KC_PGDN, KC_END,     KC_LPRN, _______,    _______, KC_RPRN, KC_LEFT,      KC_DOWN, KC_RGHT,      KC_PIPE, KC_F11,  \
   _______, KC_GRV,  C(KC_LEFT), KC_F3,   C(KC_RGHT), KC_LPRN, _______,    _______, KC_RPRN, LCA(KC_LEFT), KC_F8,   LCA(KC_RGHT), KC_BSLS, _______, \
   _______, _______, _______,    _______, _______,    _______, ADJUST,     _______, _______, _______,      _______, _______,      _______, _______  \
   ),
@@ -99,9 +99,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   */
   [_RAISE] = LAYOUT(
   _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,     _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_PPLS, _______, \
-  _______, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_LT,   _______,     _______, KC_GT,   KC_4,    KC_5,    KC_6,    KC_TILD, _______, \
-  _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_EXLM, KC_LBRC, _______,     _______, KC_RBRC, KC_1,    KC_2,    KC_3,    KC_PEQL, _______, \
-  _______, _______, _______, _______, _______, _______, _______,     ADJUST,  _______, _______, _______, _______, _______, _______  \
+  _______, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_LBRC, _______,     _______, KC_RBRC, KC_4,    KC_5,    KC_6,    KC_TILD, _______, \
+  _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_EXLM, KC_LCBR, _______,     _______, KC_RCBR, KC_1,    KC_2,    KC_3,    KC_PEQL, _______, \
+  _______, _______, _______, _______, _______, _______, _______,     ADJUST,  KC_0,    _______, _______, _______, _______, _______  \
     ),
 
   /* Adjust
@@ -122,3 +122,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______,  _______, _______, _______, _______  \
   )
 };
+
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+      REVERSE(KC_SCLN)
+      //REVERSE(KC_QUOT)
+  }
+return true;
+}
