69a276a9c13f9b5aa1b8ae0cdbc18cb9a8c7051c
tksgi <23550705+tksgi@users.noreply.github.com>
2020-12-08 22:21:03 +0900

    前回のマッピング

--- keyboards/colosseum44/rev1/keymaps/salicylic/keymap.c
+++ keyboards/colosseum44/rev1/keymaps/salicylic/keymap.c
@@ -74,9 +74,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
   [_QWERTY] = LAYOUT(
      KC_TAB, KC_Q,    KC_W,     KC_E,   KC_R,    KC_T,                    KC_Y,   KC_U,     KC_I,    KC_O,    KC_P,  KC_GRV,
-    KC_LSFT, KC_A,    KC_S,     KC_D,   KC_F,    KC_G,                    KC_H,   KC_J,     KC_K,    KC_L, KC_SCLN, KC_QUOT,
-    KC_LCTL, KC_Z,    KC_X,     KC_C,   KC_V,    KC_B,                    KC_N,   KC_M,  KC_COMM,  KC_DOT, KC_SLSH, KC_BSLS,
-                   KC_LGUI, KC_RAISE, KC_SPC, KC_BSPC,                  KC_DEL, KC_ENT, KC_LOWER, KC_LALT
+    KC_LCTL, KC_A,    KC_S,     KC_D,   KC_F,    KC_G,                    KC_H,   KC_J,     KC_K,    KC_L, KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,     KC_C,   KC_V,    KC_B,                    KC_N,   KC_M,  KC_COMM,  KC_DOT, KC_SLSH, KC_BSLS,
+                   KC_LGUI, KC_LOWER, KC_SPC, KC_BSPC,                  KC_DEL, KC_ENT, KC_RAISE, KC_LALT
   ),
     /* Lower
      */
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  KC_ESC, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                   KC_CIRC, KC_AMPR,   KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                   KC_MINS,  KC_EQL,   KC_LCBR, KC_RCBR, KC_PIPE,  KC_GRV,
   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                   KC_UNDS, KC_PLUS,   KC_LBRC, KC_RBRC, KC_BSLS, KC_TILD,
-                  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_ADJUST, KC_TRNS
+                  KC_TRNS, KC_LOWER, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_RAISE, KC_TRNS
   ),
     /* Raise
      */
@@ -93,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_ESC,  KC_1,    KC_2,      KC_3,    KC_4,    KC_5,                    KC_6,    KC_7,     KC_8,    KC_9,    KC_0, KC_BSPC,
   KC_LSFT, KC_F1,   KC_F2,     KC_F3,   KC_F4,   KC_F5,                 KC_LEFT, KC_DOWN,    KC_UP, KC_RGHT,   KC_NO,   KC_NO,
   KC_LCTL, KC_F6,   KC_F7,     KC_F8,   KC_F9,  KC_F10,                  KC_F11,  KC_F12,  KC_COMM,  KC_DOT, KC_SLSH, KC_BSLS,
-                  KC_TRNS, KC_ADJUST, KC_TRNS, KC_TRNS,                 KC_TRNS, KC_TRNS, KC_RAISE, KC_TRNS
+                  KC_TRNS, KC_LOWER, KC_TRNS, KC_TRNS,                 KC_TRNS, KC_TRNS, KC_RAISE, KC_TRNS
   ),
 
     /* Adjust
@@ -103,7 +103,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     RGB_TOG,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,              KC_WH_L, KC_WH_D,  KC_WH_U, KC_WH_R,   KC_NO,  KC_DEL,
     RGB_MOD, RGB_HUI, RGB_SAI,  RGB_VAI, RGB_SPI, KC_VOLU,              KC_MS_L, KC_MS_D,  KC_MS_U, KC_MS_R, KC_BTN1, KC_BTN2,
    RGB_RMOD, RGB_HUD, RGB_SAD,  RGB_VAD, RGB_SPD, KC_VOLD,              KC_ACL0, KC_ACL1,  KC_ACL2,   KC_NO,   KC_NO,   KC_NO,
-                      KC_TRNS, KC_RAISE, KC_TRNS, KC_TRNS,              KC_TRNS, KC_TRNS, KC_LOWER, KC_TRNS
+                      KC_TRNS, KC_LOWER, KC_TRNS, KC_TRNS,              KC_TRNS, KC_TRNS, KC_RAISE, KC_TRNS
           )
 };
 
