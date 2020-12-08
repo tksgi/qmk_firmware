a9887b8555fba2cdce1448e236c88d4736f74101
tksgi <23550705+tksgi@users.noreply.github.com>
2020-12-08 19:13:37 +0900

    keymap変更中

--- keyboards/colosseum44/rev1/keymaps/salicylic/keymap.c
+++ keyboards/colosseum44/rev1/keymaps/salicylic/keymap.c
@@ -69,78 +69,42 @@ enum macro_keycodes {
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
- 
+
    /* Qwerty
-     * ,-----------------------------------------.                ,-------------------------------------------.
-     * | Tab  |   Q  |   W  |   E  |   R  |   T  |                |   Y  |   U  |   I  |   O  |   P  |    [   |
-     * |------+------+------+------+------+------|                |------+------+------+------+------+--------|
-     * | Shift|   A  |   S  |   D  |   F  |   G  |                |   H  |   J  |   K  |   L  |   -  |    ]   |
-     * |------+------+------+------+------+------|                |------+------+------+------+------+--------|
-     * |  Ctrl|   Z  |   X  |   C  |   V  |   B  |                |   N  |   M  |   ,  |   .  |   /  |    \   |
-     * `-------------------------------------------------. ,--------------------------------------------------'
-     *               | Ctrl | MHEN/Win | ENT/LOWER|  Bsp  | | Del | spc/RAISE |HENK/Alt| Shift|
-     *               `------------------------------------' `-------------------------------'
      */
   [_QWERTY] = LAYOUT(
-        KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T,                     KC_Y, KC_U, KC_I,    KC_O,   KC_P,      JP_LBRC,
-        KC_LSFT,  KC_A, KC_S, KC_D, KC_F, KC_G,                    KC_H, KC_J, KC_K,    KC_L,   KC_MINS,   JP_RBRC,
-        KC_LCTL,  KC_Z, KC_X, KC_C, KC_V, KC_B,                    KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,   JP_ENUN,
-     LGUI(KC_V),LGUI_T(KC_MHEN), LT(_LOWER, KC_ENT), KC_BSPC,     KC_DEL, LT(_RAISE, KC_SPC),ALT_T(KC_HENK),LCTL(KC_S)
+     KC_TAB, KC_Q,    KC_W,     KC_E,   KC_R,    KC_T,                    KC_Y,   KC_U,     KC_I,    KC_O,    KC_P,  KC_GRV,
+    KC_LSFT, KC_A,    KC_S,     KC_D,   KC_F,    KC_G,                    KC_H,   KC_J,     KC_K,    KC_L, KC_SCLN, KC_QUOT,
+    KC_LCTL, KC_Z,    KC_X,     KC_C,   KC_V,    KC_B,                    KC_N,   KC_M,  KC_COMM,  KC_DOT, KC_SLSH, KC_BSLS,
+                   KC_LGUI, KC_RAISE, KC_SPC, KC_BSPC,                  KC_DEL, KC_ENT, KC_LOWER, KC_LALT
   ),
     /* Lower
-     * ,-----------------------------------------.                ,------------------------------------------.
-     * |  Esc |   !  |   ?  |   [  |   ]  |   ~  |                |   6  |   7  |   8  |   9  |   *  |   /   |
-     * |------+------+------+------+------+------|                |------+------+------+------+------+-------|
-     * |   '  |   #  |   "  |   (  |   )  |   @  |                |      |   4  |   5  |   6  |   -  |   =   |
-     * |------+------+------+------+------+------|                |------+------+------+------+------+-------|
-     * |   ^  |   %  |   &  |   ;  |   :  |   |  |                |   0  |   1  |   2  |   3  |   +  |  Ent  |
-     * `-------------------------------------------------. ,-------------------------------------------------'
-     *                      |      |ZENHAN| LOWER|       | |      | RAISE|   .  |      |
-     *                      `----------------------------' `---------------------------'
      */
   [_LOWER] = LAYOUT(
-         KC_ESC, KC_EXLM,JP_QUES,  JP_LBRC, JP_RBRC, JP_TILD,      KC_6,     KC_7,   KC_8,   KC_9, JP_ASTR, KC_SLSH,
-        JP_QUOT, KC_HASH,JP_DQUO,  JP_LPRN, JP_RPRN, JP_AT,       KC_NO,     KC_4,   KC_5,   KC_6, KC_MINS, JP_EQL,
-         JP_HAT, KC_PERC,JP_AMPR,  KC_SCLN, JP_CLON, JP_PIPE,      KC_0,     KC_1,   KC_2,   KC_3, JP_PLUS, KC_ENT,
-                         KC_TRNS,  KC_ZKHK, KC_LOWER, KC_TRNS,  KC_TRNS, KC_RAISE, KC_DOT,KC_TRNS
+ KC_ESC, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                   KC_CIRC, KC_AMPR,   KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                   KC_MINS,  KC_EQL,   KC_LCBR, KC_RCBR, KC_PIPE,  KC_GRV,
+  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                   KC_UNDS, KC_PLUS,   KC_LBRC, KC_RBRC, KC_BSLS, KC_TILD,
+                  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_ADJUST, KC_TRNS
   ),
     /* Raise
-     * ,-----------------------------------------.                ,------------------------------------------.
-     * | Esc  |   1  |   2  |   3  |   4  |   5  |                |  6   |      |  Å™  |      |  PGUP|  Bsp  |
-     * |------+------+------+------+------+------|                |------+------+------+------+------+-------|
-     * |F11/Shift| F1|  F2  |  F3  |  F4  |  F5  |                |      |  Å©  |  Å´  |  Å®  | Shift|  ENT  |
-     * |------+------+------+------+------+------|                |------+------+------+------+------+-------|
-     * |F12/Ctrl | F6|  F7  |  F8  |  F9  |  F10 |                |      |      |      |      |PGDOWN|       |
-     * `------------------------------------------------. ,-------------------------------------------------'
-     *                      |      |      | LOWER|      | |       |RAISE |      |      |
-     *                      `---------------------------' `----------------------------'
      */
 
   [_RAISE] = LAYOUT(
-         KC_ESC,  KC_1, KC_2,     KC_3,     KC_4,    KC_5,        KC_6,   KC_NO,   KC_UP,   KC_NO,  KC_PGUP, KC_BSPC,
-   SFT_T(KC_F11),KC_F1, KC_F2,   KC_F3,    KC_F4,   KC_F5,       KC_NO, KC_LEFT, KC_DOWN,KC_RIGHT,  KC_LSFT,  KC_ENT,
-  LCTL_T(KC_F12),KC_F6, KC_F7,   KC_F8,    KC_F9,  KC_F10,       KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_PGDN,   KC_NO,
-                      KC_TRNS, KC_LGUI, KC_LOWER, KC_TRNS,     KC_TRNS,KC_RAISE, KC_LALT, KC_TRNS
+   KC_ESC,  KC_1,    KC_2,      KC_3,    KC_4,    KC_5,                    KC_6,    KC_7,     KC_8,    KC_9,    KC_0, KC_BSPC,
+  KC_LSFT, KC_F1,   KC_F2,     KC_F3,   KC_F4,   KC_F5,                 KC_LEFT, KC_DOWN,    KC_UP, KC_RGHT,   KC_NO,   KC_NO,
+  KC_LCTL, KC_F6,   KC_F7,     KC_F8,   KC_F9,  KC_F10,                  KC_F11,  KC_F12,  KC_COMM,  KC_DOT, KC_SLSH, KC_BSLS,
+                  KC_TRNS, KC_ADJUST, KC_TRNS, KC_TRNS,                 KC_TRNS, KC_TRNS, KC_RAISE, KC_TRNS
   ),
 
     /* Adjust
-     * ,-----------------------------------------.                ,------------------------------------------.
-     * |  Esc |   1  |   2  |   3  |   4  |   5  |                |   6  |   7  |   8  |   9  |   0  |  Del  |
-     * |------+------+--------+------+--------+--|                |------+------+------+------+------+-------|
-     * |F11/Shift| F1|  F2  |  F3  |  F4  |  F5  |                | LTOG | LMOD |      | C A D| A Prt| PrtScn|
-     * |------+------+--------+------+--------+--|                |------+------+------+------+------+-------|
-     * |F12/Ctrl | F6|  F7  |  F8  |  F9  |  F10 |                |  VAD |  VAI |  HUD |  HUI |  SAD |  SAI  |
-     * `------------------------------------------------. ,-------------------------------------------------'
-     *                      |      |      | LOWER|      | |       | RAISE|      |      |
-     *                      `---------------------------' `----------------------------'
      */
 
   [_ADJUST] = LAYOUT(
-          KC_ESC, KC_1,  KC_2,  KC_3,   KC_4,   KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   KC_DEL,
-   SFT_T(KC_F11),KC_F1, KC_F2, KC_F3,  KC_F4,  KC_F5,       RGB_TOG, RGB_MOD,   KC_NO,LCA(KC_DEL), LALT(KC_PSCR),KC_PSCR,
-  LCTL_T(KC_F12),KC_F6, KC_F7, KC_F8,  KC_F9, KC_F10,       RGB_VAD, RGB_VAI, RGB_HUD, RGB_HUI, RGB_SAD,  RGB_SAI,
-                     KC_TRNS,KC_TRNS,KC_RAISE, KC_TRNS,    KC_TRNS, KC_LOWER, KC_TRNS,KC_TRNS
-   )
+    RGB_TOG,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,              KC_WH_L, KC_WH_D,  KC_WH_U, KC_WH_R,   KC_NO,  KC_DEL,
+    RGB_MOD, RGB_HUI, RGB_SAI,  RGB_VAI, RGB_SPI, KC_VOLU,              KC_MS_L, KC_MS_D,  KC_MS_U, KC_MS_R, KC_BTN1, KC_BTN2,
+   RGB_RMOD, RGB_HUD, RGB_SAD,  RGB_VAD, RGB_SPD, KC_VOLD,              KC_ACL0, KC_ACL1,  KC_ACL2,   KC_NO,   KC_NO,   KC_NO,
+                      KC_TRNS, KC_RAISE, KC_TRNS, KC_TRNS,              KC_TRNS, KC_TRNS, KC_LOWER, KC_TRNS
+          )
 };
 
 // define variables for reactive RGB
